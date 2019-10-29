07ba5251974511c96d665c5153f78c558e315e65
dxdx817 <45460437+dxdx817@users.noreply.github.com>
2019-10-29 10:35:49 -0400

    model for user

---
+++ cardMaestro/db/migrate/20191029142842_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :fname
+      t.string :lname
+      t.string :city
+      t.sting :email
+      t.string :enc_pwd
+      t.string :salt
+      t.boolean :public
+
+      t.timestamps
+    end
+  end
+end
