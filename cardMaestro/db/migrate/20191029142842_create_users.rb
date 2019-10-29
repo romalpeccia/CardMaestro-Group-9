34bbc8b432daf0445cee62f80de3ed312ea5e019
romalpeccia <romal.peccia@mail.utoronto.ca>
2019-10-29 10:44:58 -0400

    added master card table

--- cardMaestro/db/migrate/20191029142842_create_users.rb
+++ cardMaestro/db/migrate/20191029142842_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :fname
       t.string :lname
       t.string :city
-      t.sting :email
+      t.string :email
       t.string :enc_pwd
       t.string :salt
       t.boolean :public
