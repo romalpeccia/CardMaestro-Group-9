a5d8b71c24d9fde053ddf2f009c6620eda4ab50f
dxdx817 <45460437+dxdx817@users.noreply.github.com>
2019-11-03 15:15:15 -0500

    user signup login and authentication

--- cardMaestro/db/schema.rb
+++ cardMaestro/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_143940) do
-
+ActiveRecord::Schema.define(version: 2019_11_03_195316) do
 
   create_table "card_neededs", force: :cascade do |t|
     t.string "user"
@@ -42,16 +41,22 @@ ActiveRecord::Schema.define(version: 2019_10_29_143940) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "fname"
-    t.string "lname"
-    t.string "city"
-    t.string "email"
-    t.string "enc_pwd"
-    t.string "salt"
-    t.boolean "public"
+  create_table "tests", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
