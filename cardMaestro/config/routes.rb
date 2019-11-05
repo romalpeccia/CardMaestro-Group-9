7cdf61b36381252cceb688c0cbc1f1b7e5ca849c
lleshido <donaldlleshi@gmail.com>
2019-11-05 02:49:03 -0500

    Route changes for login,signup

--- cardMaestro/config/routes.rb
+++ cardMaestro/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
