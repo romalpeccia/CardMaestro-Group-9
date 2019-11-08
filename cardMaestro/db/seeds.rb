f5f61a549d9ce6291598b09301d36924f2773e89
romalpeccia <romal.peccia@mail.utoronto.ca>
2019-11-07 20:34:11 -0500

    updated db/seeds.rb to not add cards with missing data

--- cardMaestro/db/seeds.rb
+++ cardMaestro/db/seeds.rb
@@ -9,29 +9,27 @@ require 'mtg_sdk'
 
 #relevant info: name code release_date
 
-
+'''
 sets = MTG::Set.all
 sets.each do |set|
     if (set.online_only != true)
         CardSet.create(name: set.name, code: set.code, release_date: set.release_date)
-        '''
-        puts set.name 
-        puts set.code
-        puts set.release_date
-        '''
+
     end
 end
-
+'''
 
 
 #cards = MTG::Card.where(page: 5).where(pageSize: 10).all
 cards = MTG::Card.all
 cards.each do |card|
-
-    card_set = CardSet.find_by(code: card.set)
-    card_set.card.create(name: card.name, set: card.set, image_url: card.image_url)
-
-    #Card.create(name: card.name, set: card.set, image_url: card.image_url)
+    if card
+        if (card.image_url)
+            card_set = CardSet.find_by(code: card.set)
+            card_set.card.create(name: card.name, set: card.set, image_url: card.image_url)
+            Card.create(name: card.name, set: card.set, image_url: card.image_url)
+        end
+    end
     '''
     puts card.name
     puts card.set
