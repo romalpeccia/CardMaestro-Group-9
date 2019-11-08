101df45f405bf9514a84b6413ca3ced0d87b7199
lleshido <donaldlleshi@gmail.com>
2019-11-08 13:36:10 -0500

    Implementation of search functionality

--- cardMaestro/app/controllers/searchpage_controller.rb
+++ cardMaestro/app/controllers/searchpage_controller.rb
@@ -5,6 +5,8 @@ class SearchpageController < ApplicationController
   def search
     #obtain the search term
     search_term = params[:param1]
-    
+    current_user_param = params[:param2]
+    @results = CardOwned.all.where("lower(card_name) LIKE lower(:search) and user_id <> :current_user_id", search: "%#{search_term}%", current_user_id: current_user_param )
+
   end
 end
--- cardMaestro/app/controllers/welcome_controller.rb
+++ cardMaestro/app/controllers/welcome_controller.rb
@@ -4,7 +4,8 @@ class WelcomeController < ApplicationController
     #if search query, redirect to search controller
     if params[:search]
       search_term = params[:search]
-      redirect_to searchpage_search_path(:param1 =>search_term) 
+      current_user_param = current_user.id
+      redirect_to searchpage_search_path(:param1 =>search_term, :param2 =>current_user_param) 
     end
   end
 
