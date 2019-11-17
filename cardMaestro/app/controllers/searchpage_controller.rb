d4c5c026e903ae70f9ed0e24f210f870d09d5eca
lleshido <donaldlleshi@gmail.com>
2019-11-17 00:06:49 -0500

    Searching now works on all pages

--- cardMaestro/app/controllers/searchpage_controller.rb
+++ cardMaestro/app/controllers/searchpage_controller.rb
@@ -1,11 +1,12 @@
 class SearchpageController < ApplicationController
-  def index
-  end
+  
 
   def search
+    
+    search_term = params[:search]
+    current_user_param = current_user.id
+    
     #obtain the search term
-    search_term = params[:param1]
-    current_user_param = params[:param2]
     @results = CardOwned.all.where("lower(card_name) LIKE lower(:search) and user_id <> :current_user_id", search: "%#{search_term}%", current_user_id: current_user_param )
 
   end
--- cardMaestro/app/controllers/welcome_controller.rb
+++ cardMaestro/app/controllers/welcome_controller.rb
@@ -1,13 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  
-    #if search query, redirect to search controller
-    if params[:search]
-      search_term = params[:search]
-      current_user_param = current_user.id
-      redirect_to searchpage_search_path(:param1 =>search_term, :param2 =>current_user_param) 
-    end
   end
+
 
 
 end
