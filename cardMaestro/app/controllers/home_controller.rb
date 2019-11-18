eb8ab2847b77ff9ce0d45a39e8b7a4284d987c30
lleshido <donaldlleshi@gmail.com>
2019-11-18 03:52:36 -0500

    Added carousel UI to home page

--- cardMaestro/app/controllers/home_controller.rb
+++ cardMaestro/app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
     def index
+    
+    user_id = current_user.id
+    @collection_error = false
+
+    target_user = User.find_by(id: user_id)
+    if target_user
+      @target_email = target_user.email
+      @collection_card_owned = target_user.card_owned
+      @collection_card_needed = target_user.card_needed
+      
+    else
+      flash.now[:alert] = "User Not Found"
+      @collection_error = true
+    end
     end
 end
