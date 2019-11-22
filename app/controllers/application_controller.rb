cff14dd6b141cb1b65707326d6392a35c0229992
Yizhi Xu <yizhi.xu@mail.utoronto.ca>
2019-11-22 16:10:29 -0500

    add location features for signup page back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,12 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        # devise_parameter_sanitizer.permit(:sign_up, keys: [:city])
-        # devise_parameter_sanitizer.permit(:account_update, keys: [:city])
-        # devise_parameter_sanitizer.permit(:sign_up, keys: [:province])
-        # devise_parameter_sanitizer.permit(:account_update, keys: [:province])
-        # devise_parameter_sanitizer.permit(:sign_up, keys: [:country])
-        # devise_parameter_sanitizer.permit(:account_update, keys: [:country])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:city])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:city])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:province])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:province])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:country])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:country])
     end
 
 
