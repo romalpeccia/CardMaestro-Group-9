b813d4e3966ff7973c9e1de2c6c6938ef6336ecd
dxdx817 <45460437+dxdx817@users.noreply.github.com>
2019-11-24 18:30:45 -0500

    renew the db

--- db/schema.rb
+++ db/schema.rb
@@ -114,8 +114,8 @@ ActiveRecord::Schema.define(version: 2019_11_24_011637) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "city"
-    t.string "province2"
-    t.string "country2"
+    t.string "province"
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
