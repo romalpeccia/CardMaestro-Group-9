7b9b81b94e1adcd4d49e1c6f8420a2256e639cd8
romalpeccia <romal.peccia@mail.utoronto.ca>
2019-11-25 21:54:07 -0500

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2019_11_24_011637) do
-=======
-ActiveRecord::Schema.define(version: 2019_11_24_001456) do
->>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
