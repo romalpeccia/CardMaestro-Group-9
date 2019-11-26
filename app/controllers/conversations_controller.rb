6063beab1c31a7ab68fbfc8061b75e6f71b77e2d
lleshido <donaldlleshi@gmail.com>
2019-11-26 15:23:08 -0500

    inbox page functionality, UI changes in progress

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,37 @@
 class ConversationsController < ApplicationController
     before_action :authenticate_user!
+    before_action do
+      #@conversation = Conversation.find(params[:conversation_id])
+    end
   
     def index
       @users = User.all
       @conversations = Conversation.all
+
+    
+       #needs to be removed TODO
+       @conversation = Conversation.find(5)
+       @messages = @conversation.messages
+       @message = @conversation.messages.new
+      
+    end
+
+    def show
+      @users = User.all
+      @conversations = Conversation.all
+
+    
+       #needs to be removed TODO
+       @conversation = Conversation.find(4)
+       @messages = @conversation.messages
+       @message = @conversation.messages.new
+
+      render 'index'
+
+    end
+
+    def new_message
+      @message = @conversation.messages.new
     end
   
     def create
@@ -14,9 +42,20 @@ class ConversationsController < ApplicationController
       end
       redirect_to conversation_messages_path(@conversation)
     end
+
+    def create_message
+      @message = @conversation.messages.new(message_params)
+      if @message.save
+        redirect_to conversation_messages_path(@conversation)
+      end
+    end
   
     private
     def conversation_params
       params.permit(:sender_id, :recipient_id)
+    end
+
+    def message_params
+      params.require(:message).permit(:body, :user_id)
     end
   end
