13aa531c11064f800239344d31c9c9d3c4bda40b
dxdx817 <45460437+dxdx817@users.noreply.github.com>
2019-11-28 22:25:47 -0500

    fixed the bug in site map and add a .env gem try to send the email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   #devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
