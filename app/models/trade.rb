99448a274970bf11d2985233ef0ab36a0846c5f1
lleshido <donaldlleshi@gmail.com>
2019-11-29 18:57:34 -0500

    added association for ratings and trades

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -5,6 +5,8 @@ class Trade < ApplicationRecord
     has_many :sender_cards, :class_name => "CardOffer", :foreign_key => 'sender_cards_id'
     has_many :reciever_cards, :class_name => 'CardOffer', :foreign_key => 'reciever_cards_id'
 
+    has_many :ratings
+
     validates :sender_value, :numericality => { :greater_than_or_equal_to => 0 }
     validates :reciever_value, :numericality => { :greater_than_or_equal_to => 0 }
 end
