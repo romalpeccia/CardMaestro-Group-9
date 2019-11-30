2239cefc98c6af51d055665771f42e9544c0cc5d
dxdx817 <45460437+dxdx817@users.noreply.github.com>
2019-11-30 17:42:52 -0500

    change the host to test on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,8 +103,8 @@ Rails.application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password:ENV["GMAIL_PASSWORD"]
   }
-# doesn't have to be Heroku, but you get the idea.
-config.action_mailer.default_url_options = { :host => 'cardmaestro-group-9.herokuapp.com/' }
+
+  config.action_mailer.default_url_options = { :host => 'https://cardmaestro-group-9.herokuapp.com/' }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
