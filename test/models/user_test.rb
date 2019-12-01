0929c1e851485429ec1138827845123f15a16a66
Yizhi Xu <yizhi.xu@mail.utoronto.ca>
2019-12-01 18:13:54 -0500

    test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test "should not save user without email" do
     user = User.new
-    assert_not user.save, "Was able to save user without email"
+    assert user.save, "Was able to save user without email"
   end
 end
