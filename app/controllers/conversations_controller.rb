1ce602295ea536d7a28128a7da10db2d9f27e3d6
lleshido <donaldlleshi@gmail.com>
2019-12-01 14:00:21 -0500

    inbox page users can send messages to each other

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,18 +1,8 @@
 class ConversationsController < ApplicationController
-    before_action :authenticate_user!
-    before_action do
-      #@conversation = Conversation.find(params[:conversation_id])
-    end
   
     def index
       @users = User.all
       @conversations = Conversation.all
-
-    
-       #needs to be removed TODO
-       #@conversation = Conversation.find(params[:id])
-       #@messages = @conversation.messages
-       #@message = @conversation.messages.new
       
     end
 
@@ -23,6 +13,7 @@ class ConversationsController < ApplicationController
     
        #needs to be removed TODO
        @conversation = Conversation.find(params[:id])
+       @conversation_id = @conversation.id
        @messages = @conversation.messages
        @message = @conversation.messages.new
 
@@ -31,23 +22,29 @@ class ConversationsController < ApplicationController
     end
 
     def new_message
+      
       @message = @conversation.messages.new
     end
   
     def create
+      
       if Conversation.between(params[:sender_id], params[:recipient_id]).present?
         @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
       else
         @conversation = Conversation.create!(conversation_params)
       end
-      redirect_to conversation_messages_path(@conversation)
+      #redirect_to conversation_messages_path(@conversation)
     end
 
     def create_message
-      @message = @conversation.messages.new(message_params)
+      
+      @conversation = Conversation.find(params[:id])
+      @message = @conversation.messages.new(params["/conversations"].permit(:body, :user_id))
+    
       if @message.save
-        redirect_to conversation_messages_path(@conversation)
-      end
+        id = @message.id
+         redirect_to :controller => 'conversations', :action => 'show', :anchor => id
+       end
     end
   
     private
@@ -55,7 +52,6 @@ class ConversationsController < ApplicationController
       params.permit(:sender_id, :recipient_id)
     end
 
-    def message_params
-      params.require(:message).permit(:body, :user_id)
-    end
+
+    
   end
